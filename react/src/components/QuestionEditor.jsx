a7a46acb471acc3ca01fc360f4c44a56ef920b60
Azhar Uddin <azhar@sender.net>
2024-04-02 03:37:00 +0600

    type mismatch

--- react/src/components/QuestionEditor.jsx
+++ react/src/components/QuestionEditor.jsx
@@ -140,7 +140,7 @@ export default function QuestionEditor({
               className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
             >
               {questionTypes.map((type) => (
-                <option value={upperCaseFirst(type)} key={type}>
+                <option value={type} key={type}>
                   {upperCaseFirst(type)}
                 </option>
               ))}
